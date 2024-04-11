f44c5d7283df5d8e869cbbdf4ed3f8d955413d95
Katona Andrea <katonaandreaizabella914@gmail.com>
2024-04-11 13:46:18 +0300

    rectangle draw + delete

---
+++ ParkingSpacePicker.py
@@ -0,0 +1,30 @@
+import cv2
+import pickle
+
+
+
+width, height = 107, 48
+posList = []
+
+
+def mouseClick(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+
+    if event == cv2.EVENT_RBUTTONDOWN:
+        for i,pos in enumerate(posList):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1+ height:
+                posList.pop(i)
+
+
+
+
+while True:
+    img = cv2.imread('carParkImg.png')
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0]+width, pos[1]+height), (255, 0, 0), 2)
+
+    cv2.imshow("image", img)
+    cv2.setMouseCallback("image", mouseClick)
+    cv2.waitKey(1)
