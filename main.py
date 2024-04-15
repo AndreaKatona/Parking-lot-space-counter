64b506dae781b8781f6f7d250a4eb60537f3e09a
Katona Andrea <katonaandreaizabella914@gmail.com>
2024-04-15 15:43:57 +0300

    free parking space counter added

--- main.py
+++ main.py
@@ -11,14 +11,28 @@ with open('CarParkPos', 'rb') as fin:
 width, height = 107, 48
 
 
-def checkParkingSpace():
+def checkParkingSpace(imgProcess):
+
+    spaceCounter = 0
     for pos in posList:
         x, y = pos
-        cv2.rectangle(img, pos, (pos[0]+width, pos[1]+height), (255, 0, 0), 2)
-        
-        imgCrop = img[y:y+height, x:x+width]
-        cv2.imshow(str(x*y), imgCrop)
 
+        imgCrop = imgProcess[y:y+height, x:x+width]
+        # cv2.imshow(str(x*y), imgCrop)
+        count = cv2.countNonZero(imgCrop)
+        #cvzone.putTextRect(img, str(count), (x, y+height-5),
+        #                   scale=1, thickness=1, offset=0)
+
+        if count < 900:
+            color = (0, 255, 0)
+            spaceCounter += 1 
+        else:
+            color = (0, 0, 255)
+
+        cv2.rectangle(img, pos, (pos[0]+width, pos[1]+height), color, 2)
+        
+    cvzone.putTextRect(img, f'Free: {str(spaceCounter)}/{len(posList)}', (100, 50),
+                           scale=3, thickness=5, offset=20, colorR=(0,200,0))
 
 while True:
 
@@ -26,7 +40,18 @@ while True:
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
     success, img = cap.read()
-    checkParkingSpace()
+
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+    imgThreshold = cv2.adaptiveThreshold(
+        imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
+
+    imgMedian = cv2.medianBlur(imgThreshold, 5)
+    kernel = np.ones((3, 3), np.int8)
+    imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
+
+    checkParkingSpace(imgDilate)
 
     cv2.imshow("Image", img)
+    # cv2.imshow("ImageGray", imgMedian)
     cv2.waitKey(10)
